e7ab5f69e78b2ef2fef8b288b2beccd1475ebb12
mihai3001 <rider.white26@yahoo.com>
2020-07-20 00:26:50 +0300

    Updates

---
+++ precache-manifest.7517a5407b7f777830eaf3d2830a197c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e718fd0974686b8bb1d6024beab0ed3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d2212c32898e92527117",
+    "url": "/monster-rolodex/static/css/main.d346a57e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2212c32898e92527117",
+    "url": "/monster-rolodex/static/js/main.f3e80181.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
